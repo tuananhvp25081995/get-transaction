b8d081deac2fc13fba62587849de4dbc88f0c3a9
Tuan Anh <tuananhvp25895@gmail.com>
2021-07-26 16:33:00 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+require('dotenv').config()
+const cors = require('cors')
+const rateLimit = require("express-rate-limit");
+const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const apiLimiter = rateLimit({
+    windowMs: 10 * 1000,
+    max: 1,
+    message: "Too many request, please slowly"
+});
+
+require("./bot")
+
+app.use(cors())
+    app.listen(5000, function () {
+    console.log('CORS-enabled web server listening on port 5000')
+})
+
+module.exports = app
---
+++ bot.js
@@ -0,0 +1,61 @@
+const Web3 = require('web3');
+require('dotenv').config()
+const axios = require("axios").default;
+
+class TransactionChecker {
+    web3;
+    web3ws;
+    account;
+    subscription;
+
+    constructor(projectId, account) {
+        this.web3ws = new Web3(new Web3.providers.WebsocketProvider('wss://data-seed-prebsc-1-s1.binance.org:8545/'));
+        this.web3 = new Web3(new Web3.providers.HttpProvider('https://data-seed-prebsc-1-s1.binance.org:8545/'));
+        this.account = account.toLowerCase();
+    }
+
+    subscribe(topic) {
+        this.subscription = this.web3ws.eth.subscribe(topic, (err, res) => {
+            if (err) console.error(err);
+        });
+    }
+
+    watchTransactions() {
+        console.log('Watching all pending transactions...');
+        this.subscription.on('data', (txHash) => {
+            setTimeout(async () => {
+                try {
+                    let tx = await this.web3.eth.getTransaction(txHash);
+                    if (tx != null) {
+                        if (this.account == tx.to.toLowerCase()) {
+                            const WebhookId = this.account
+                            let address =  tx.from
+                            let value = this.web3.utils.fromWei(tx.value, 'ether')
+                            var time =  new Date();
+                            console.log(address,value,time)
+                            axios
+                                .post(`http://localhost:3001/webhook/hook-bsc/${WebhookId}`, {
+                                    address,
+                                    value,
+                                    time
+                                })
+                                .then(function (response) {
+                                    result = response.data;
+                                    console.log("ok", result);
+                                })
+                                .catch(function (error) {
+                                    console.error(error);
+                                });
+                        }
+                    }
+                } catch (err) {
+                    console.error(err);
+                }
+            }, 15000)
+        });
+    }
+}
+
+let txChecker = new TransactionChecker(process.env.INFURA_ID, '0x9e5B17d8D46B4Ba4e5F82A91628C27Fd162EbA2b');
+txChecker.subscribe('pendingTransactions');
+txChecker.watchTransactions();
